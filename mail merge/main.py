38cdf002ee5e13e2ddd94c19730ed58a6ca03141
jphasha <jphasha@student.wethinkcode.co.za>
2024-11-06 23:30:05 +0200

    finished

--- mail merge/main.py
+++ mail merge/main.py
@@ -6,3 +6,19 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+def create_list():
+    f =  open("mail merge/Input/Names/inivited_names.txt", "r")
+    names =f.readlines()
+    names2 = []
+    for x in names:
+        if x:
+            names2.append(x.strip())
+    
+    docx = open('mail merge/Input/Letters/starting_letter.docx', 'r')
+    docx2 = docx.read()
+    for x in names2:
+        file = docx2.replace('name', x)
+        file2 = open(f"mail merge/Input/{x}.txt", "w")
+        file2.write(file)
+create_list()
